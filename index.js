20ba6f7af0eb5fd1775c54657d96d971a8d1ea5b
Stephane <stephanevale336@gmail.com>
2024-09-13 14:00:02 -0300

    Modifica botões nos guichês

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fila = [];
+let fila = [];
 
 const categorias = [
     { nome: "Agendamentos", id: 1, codigo: "AG" },
@@ -9,9 +9,12 @@ const categorias = [
 ];
 
 const guiches = [
-    { id: 1, categoriasId: [1, 2, 3] },
-    { id: 2, categoriasId: [4, 5] },    
-]
+    {
+        id: 1,
+        categoriasId: [categorias[0].id, categorias[1].id, categorias[2].id],
+    },
+    { id: 2, categoriasId: [categorias[3].id, categorias[4].id]},
+];
 
 $(document).ready(() => {
     const listagemCategorias = $("#listagem-categorias");
@@ -21,20 +24,53 @@ $(document).ready(() => {
         );
         listagemCategorias.append(itemCategoria);
     });
+
     const botoesSenha = $(".botao-senha");
     botoesSenha.on("click", function () {
-        geraSenha($(this).attr("data-id"));
+        const categoriaId = $(this).attr("data-id");
+        geraSenha(parseInt(categoriaId));
     });
-    function geraSenha(categoriaId) {
-        const categoria = categorias.find(function(cat){
-            return cat.id === parseInt(categoriaId);
+
+    const chamaSenha = $(".botao");
+    chamaSenha.on("click", function () {
+        const guicheId = $(this).attr("data-guiche-id");
+        const guiche = guiches.find(function (gui) {
+            return gui.id === parseInt(guicheId);
         });
-        console.log(categoria.codigo);
+        const senhaChamada = fila.find(function (senha) {
+            return guiche.categoriasId.includes(senha.categoriaId);
+        });
+
+        if (senhaChamada) {
+            enviaSenhaChamadaParaGuiche(senhaChamada, guicheId)
+        }
+    });
+
+    function geraSenha(categoriaId) {
+        const categoria = categorias.find(function (cat) {
+            return cat.id === categoriaId;
+        });
         const numero = 1 + Math.floor(Math.random() * (100 - 1) + 1);
         const senhaAleatoria = `${categoria.codigo}${numero}`;
-        fila.push(senhaAleatoria);
+
+        fila.push({ categoriaId, senha: senhaAleatoria });
+
         const listagemSenhas = $("#listagemSenhasGeradas");
         const senhaGerada = $(`<li class="item-fila">${senhaAleatoria}</li>`);
         listagemSenhas.append(senhaGerada);
+    }
+
+    function enviaSenhaChamadaParaGuiche(senhaChamada, guicheId) {
+        fila = fila.filter(function(item) {
+            return item.senha !== senhaChamada.senha
+        })
+        const listagemSenhas = $("#listagemSenhasGeradas");
+        listagemSenhas.html('')
+        fila.forEach((item) => {
+            const senhaGerada = $(`<li class="item-fila">${item.senha}</li>`);
+            listagemSenhas.append(senhaGerada);
+        })
+        const exibicaoguiche = $(`#senhaguiche${guicheId}`);
+        exibicaoguiche.html(senhaChamada.senha);
     }
 });
